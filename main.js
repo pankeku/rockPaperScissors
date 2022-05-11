c3831156aef85d84bfe43aeb671747c15fcfe6dd
pankeku <drwagw@gmail.com>
2022-05-11 16:28:18 +0300

    Add buttons in UI and game acts according to pressed button

--- main.js
+++ main.js
@@ -1,81 +1,89 @@
+let playerScore = 0;
+let computerScore = 0;
+let roundNumber = 1;
+let computerChoice = "";
+let playerChoice = "";
+
 function computerPlay() {
-    const max = 3;
-    let number = Math.floor(Math.random() * max);
-    console.log("*Random number: " + number);
+  const max = 3;
+  let number = Math.floor(Math.random() * max);
 
-    let play;
+  let play;
 
-    switch (number) {
-        case 0:
-            play = "rock";
-            break;
-        case 1: 
-            play = "paper";
-            break;
-        case 2:
-            play = "scissors"
-            break;
-    }
-    return play;
-    }
-
-function playerSelection() {
-    let selection = "";
-
-    while (selection !== "rock" && selection !== "paper" && selection !== "scissors") {
-        selection = prompt("Rock, paper or scissors?").toLowerCase();
-    }
-    return selection;
+  switch (number) {
+    case 0:
+      play = "rock";
+      break;
+    case 1:
+      play = "paper";
+      break;
+    case 2:
+      play = "scissors";
+      break;
+  }
+  return play;
 }
 
 function playRound(playerSelection, computerPlay) {
+  playerChoice = playerSelection;
+  computerChoice = computerPlay;
 
-    if (playerSelection == computerPlay) {
-        return 0;
-    } else if (playerSelection == "rock" && computerPlay == "scissors") {
-            return 1;
-        } else if (playerSelection == "scissors" && computerPlay == "paper") {
-            return 1;
-         } else if (playerSelection == "paper" && computerPlay == "rock") {
-             return 1;
-         } else {
-             return 2;
-         }
-        }
-
-
-function game() {
-
-    let playerScore = 0;
-    let computerScore = 0;
-    let roundNumber = 1;
-    
-    while (playerScore < 5 && computerScore < 5) {
-
-    
-    let playerChoice = playerSelection();
-    let computerChoice = computerPlay();
-
-        if (playRound(playerChoice, computerChoice) == 0) {
-            console.log(`Round #${roundNumber}: It's a draw. Your choice: ${computerChoice}, computer's choice: ${playerChoice}. Score: ${playerScore}:${computerScore}.`);
-            roundNumber++;
-
-        } else if (playRound(playerChoice, computerChoice) == 1) {
-            playerScore++;
-            console.log(`Round #${roundNumber}: You win. Your choice - ${playerChoice}, beats computer's choice - ${computerChoice}. Score: ${playerScore}:${computerScore}.`);
-            roundNumber++;
-
-        } else if (playRound(playerChoice, computerChoice) == 2) {
-            computerScore++;
-            console.log(`Round #${roundNumber}: You lose. Computer's choice - ${computerChoice}, beats your choice - ${playerChoice}. Score: ${playerScore}:${computerScore}.`);
-            roundNumber++;
-        };
-
-        if (playerScore == 5) {
-            console.log(`You win the game. Score: ${playerScore}:${computerScore}.`)
-        } else if (computerScore == 5) {
-            console.log(`You lost the game. Score: ${playerScore}:${computerScore}.`)
-        }
-    }
-
+  // draw
+  if (playerSelection == computerPlay) {
+    game(0);
+    //all player's winning combinations
+  } else if (playerSelection == "rock" && computerPlay == "scissors") {
+    game(1);
+  } else if (playerSelection == "scissors" && computerPlay == "paper") {
+    game(1);
+  } else if (playerSelection == "paper" && computerPlay == "rock") {
+    game(1);
+    //else combinations are lost
+  } else {
+    game(2);
+  }
 }
+
+function game(roundResult) {
+  if (roundResult == 0) {
+    console.log(
+      `Round #${roundNumber}: It's a draw. Your choice: ${computerChoice}, computer's choice: ${playerChoice}. Score: ${playerScore}:${computerScore}.`
+    );
+    roundNumber++;
+  } else if (roundResult == 1) {
+    playerScore++;
+    console.log(
+      `Round #${roundNumber}: You win. Your choice - ${playerChoice}, beats computer's choice - ${computerChoice}. Score: ${playerScore}:${computerScore}.`
+    );
+    roundNumber++;
+  } else if (roundResult == 2) {
+    computerScore++;
+    console.log(
+      `Round #${roundNumber}: You lose. Computer's choice - ${computerChoice}, beats your choice - ${playerChoice}. Score: ${playerScore}:${computerScore}.`
+    );
+    roundNumber++;
+  }
+
+  if (playerScore == 5) {
+    console.log(`You win the game. Score: ${playerScore}:${computerScore}.`);
+    resetResults();
+  } else if (computerScore == 5) {
+    console.log(`You lost the game. Score: ${playerScore}:${computerScore}.`);
+    resetResults();
+  }
+}
+
+function resetResults() {
+  playerScore = 0;
+  computerScore = 0;
+  roundNumber = 1;
+}
+
+let buttons = document.querySelectorAll("button");
+console.log(buttons);
+
+buttons.forEach((button) =>
+  button.addEventListener("click", () => {
+    playRound(button.id, computerPlay());
+  })
+);
